44e17e8b838bcfb819cc4f9982ee1a208a1455c3
sean <maherseanmilo@gmail.com>
2021-01-19 11:43:39 -0600

    adds song data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Song.create(title: "Bananas and Blow", album: "White Pepper", artist: "Ween", year: 1997)
+Song.create(title: "Sample in a Jar", album: "Hoist", artist: "Phish", year: 1994)
+Song.create(title: "Feathered Indians", album: "Purgatory", artist: "Tyler Childers", year: 2013)
+Song.create(title: "Hangin' on the Telephone", album: "Parallel Lines", artist: "Blondie", year: 1977)
+Song.create(title: "Manzanita", album: "Manzanita", artist: "Tony Rice", year: 1977)
